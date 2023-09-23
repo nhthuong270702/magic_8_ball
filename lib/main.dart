64b9a6986be4a8adbdafd1a79ad5eaee74329ca0
NguyenHuuThuong <bimax270702@gmail.com>
2023-09-23 16:41:05 +0700

    push code

---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+  MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.deepPurple,
+      appBar: AppBar(
+        title: Text('8 balls'),
+        backgroundColor: Colors.deepPurpleAccent,
+      ),
+      body: Balls(),
+    ),
+  ),
+);
+
+class Balls extends StatefulWidget {
+  @override
+  _BallsState createState() => _BallsState();
+}
+
+class _BallsState extends State<Balls> {
+  int choice = 1;
+
+  void update() {
+    setState(() {
+      choice = Random().nextInt(5) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Expanded(
+        child: TextButton(
+          onPressed: update,
+          // Call the rollDice function when button is pressed
+          child: Image.asset('images/ball$choice.png'),
+        ),
+      ),
+    );
+  }
+}
